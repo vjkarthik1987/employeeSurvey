b626ba4ddc817c12b793b1f0762154538e893a32
vjkarthik1987 <vjkarthik.2987@gmail.com>
2025-02-23 08:22:01 +0530

    Options reversed, useNewUrlParser and useUnifiedTopology removed

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/pricePelican", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log("MongoDB connection error:", err));
+mongoose.connect("mongodb://127.0.0.1:27017/pricePelican")
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("MongoDB connection error:", err));
 
 module.exports = mongoose;
